dc2cd7e63b8ebeba76f926adc4cef9a86b54cd1e
Yuliia Shaparenko <shaparenko@cip.ifi.lmu.de>
2021-05-19 11:47:58 +0200

    Login Controller outline added

--- src/main/java/client/view/LoginController.java
+++ src/main/java/client/view/LoginController.java
@@ -1,8 +1,24 @@
 package client.view;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 /**
  * @author can ren
+ * @author yuliia shaparenko
  * @create $(YEAR)-$(MONTH)-$(DAY)
  */
 public class LoginController {
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Button startButton;
+
+//    @FXML
+//    private void loginButton{
+//
+//    }
+
 }
