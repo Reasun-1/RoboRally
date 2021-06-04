f1b5156eb36b5e71013ae951fae738c9435f622d
ren <ren@cip.ifi.lmu.de>
2021-06-04 15:40:22 +0200

    Update: Gridpane für Robot ergänzt.

--- src/main/java/client/view/ChatController.java
+++ src/main/java/client/view/ChatController.java
@@ -45,6 +45,8 @@ public class ChatController {
     @FXML
     private GridPane gridPaneBoard;
     @FXML
+    private GridPane gridPaneRobot;
+    @FXML
     private TextField messageField; //bind the typed message with message history scroll pane
     @FXML
     private TextField information; // bind INFORMATION StringProperty in Client
@@ -629,10 +631,10 @@ public class ChatController {
 
 
     public void testBoardButtonEvent(){
-        ImageView boardElemen = new ImageView(imageCheckpoint1);
+        ImageView boardElemen = new ImageView(HulkBot);
         boardElemen.setFitHeight(43);
         boardElemen.setFitWidth(43);
         //boardElemen.setRotate(boardElemen.getRotate() + 90);
-        gridPaneBoard.add(boardElemen,5,1);
+        gridPaneRobot.add(boardElemen,5,1);
     }
 }
