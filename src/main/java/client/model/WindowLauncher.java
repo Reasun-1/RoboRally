8d0b7f8224cd7f28315d6f7f9a2158626052a2fa
Chiara Welz <c.welz@campus.lmu.de>
2021-06-11 10:58:48 +0200

    FXML 1229x779px

--- src/main/java/client/model/WindowLauncher.java
+++ src/main/java/client/model/WindowLauncher.java
@@ -40,7 +40,7 @@ public class WindowLauncher {
         Parent root = loader.load();
         ChatController ctrl = loader.getController();
         ctrl.init(client);
-        stage.setScene(new Scene(root, 1342, 800));
+        stage.setScene(new Scene(root, 1226, 779));
         stage.show();
         stage.setOnCloseRequest((event) -> Platform.exit());
     }
