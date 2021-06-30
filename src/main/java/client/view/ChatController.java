d5db43a62c13b44d8000cd17ff54780136e8513d
ren <ren@cip.ifi.lmu.de>
2021-06-30 18:28:39 +0200

    Update: Map Twister von Jonas ergänzt:)

--- src/main/java/client/view/ChatController.java
+++ src/main/java/client/view/ChatController.java
@@ -239,7 +239,7 @@ public class ChatController {
             public void changed(ObservableValue<? extends ObservableList<String>> observableValue, ObservableList<String> strings, ObservableList<String> t1) {
                 ObservableList<String> mapObsList = client.getMAPS();
                 System.out.println("in Controller " + mapObsList);
-                if (mapList.getItems().size() < 4) {
+                if (mapList.getItems().size() < 5) {
                     mapList.getItems().clear();
                     mapList.getItems().addAll(mapObsList);
                 }
