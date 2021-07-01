70d514c176a2ed73f62cbac0626e3175a2c03589
skrrt420 <73880635+skrrt420@users.noreply.github.com>
2021-07-01 23:35:55 +0200

    memorySwap Window erstellt (protocol-update änderungen schon gepulled) FXML Attribute im Controller Klasse erstellt

---
+++ src/main/java/client/view/MemorySwapController.java
@@ -0,0 +1,60 @@
+package client.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
+
+public class MemorySwapController {
+
+    @FXML
+    private Button card1Button;
+    @FXML
+    private Button card2Button;
+    @FXML
+    private Button card3Button;
+    @FXML
+    private Button card4Button;
+    @FXML
+    private Button card5Button;
+    @FXML
+    private Button card6Button;
+    @FXML
+    private Button card7Button;
+    @FXML
+    private Button card8Button;
+    @FXML
+    private Button card9Button;
+    @FXML
+    private Button card10Button;
+    @FXML
+    private Button card11Button;
+    @FXML
+    private Button card12Button;
+    @FXML
+    private ImageView card1;
+    @FXML
+    private ImageView card2;
+    @FXML
+    private ImageView card3;
+    @FXML
+    private ImageView card4;
+    @FXML
+    private ImageView card5;
+    @FXML
+    private ImageView card6;
+    @FXML
+    private ImageView card7;
+    @FXML
+    private ImageView card8;
+    @FXML
+    private ImageView card9;
+    @FXML
+    private ImageView card10;
+    @FXML
+    private ImageView card11;
+    @FXML
+    private ImageView card12;
+    @FXML
+    private Label chooseCards;
+}
